b7753262fd3d558aba8dfbbe6b29b55443233c74
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-03-03 16:48:16 -0300

    [feat] add query 02

--- challenges/desafio2.sql
+++ challenges/desafio2.sql
@@ -1,1 +1,10 @@
-
+SELECT
+    COUNT(DISTINCT musicas.id) cancoes,
+    COUNT(DISTINCT artistas.id) artistas,
+    COUNT(DISTINCT albuns.id) albuns
+FROM
+    albuns
+        INNER JOIN
+    musicas
+        INNER JOIN
+    artistas;
