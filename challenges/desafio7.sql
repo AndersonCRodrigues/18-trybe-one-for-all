089776697c326437f664412decf4702df2d722ae
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-03-03 16:46:31 -0300

    [feat] add db script

---
+++ challenges/desafio7.sql
@@ -0,0 +1,12 @@
+SELECT
+    (ar.nome) artista,
+    (al.title) album,
+    COUNT(us.artista_id) pessoas_seguidoras
+FROM
+    artistas ar
+        INNER JOIN
+    albuns al ON ar.id = al.artista_id
+        INNER JOIN
+    usuario_segue us ON us.artista_id = ar.id
+GROUP BY ar.nome , al.title
+ORDER BY COUNT(us.artista_id) DESC, ar.nome;
